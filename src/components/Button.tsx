0558a872183fd870366dbb34800fa2b8f559ef50
genedeh <genesis.enedeh@gmail.com>
2025-12-23 23:59:03 +0100

    feat: Implement Hero section

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react"
+
+interface Props {
+    id: string
+    title: string
+    containerClass: string
+    leftIcon?: ReactNode
+    rightIcon?: ReactNode
+
+}
+const Button = ({ title, id, containerClass, leftIcon, rightIcon }: Props) => {
+    return (
+        <button id={id}
+            className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-(--violet-50) text-black px-7 py-3 ${containerClass}`}>
+            {leftIcon}
+            <span className="relative incline-flex overflow-hidden font-(--font-general) text-xs uppercase">{title}</span>
+            {rightIcon}
+        </button>
+    )
+}
+
+export default Button
